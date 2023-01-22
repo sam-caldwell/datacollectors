296a13bd8f2325f3f08573c65564a899118ae5e9
Sam Caldwell <github@samcaldwell.net>
2023-01-22 15:08:37 -0600

    comments for toolkit schema

--- data/toolkit/sql/0000-create-schema.sql
+++ data/toolkit/sql/0000-create-schema.sql
@@ -1,1 +1,6 @@
+/*
+ * The sql/toolkit project aims to create common easy-to-use
+ * sql functions/procedures and other objects which can
+ * be used by other databases which use toolkit db as a template.
+ */
 create schema if not exists toolkit;
