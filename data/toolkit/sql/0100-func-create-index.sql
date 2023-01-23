27dce7b5e9d29188bbfd5eb4c2695610e64b696f
Sam Caldwell <github@samcaldwell.net>
2023-01-22 21:25:34 -0600

    fix bug in create/delete index function tests

--- data/toolkit/sql/0100-func-create-index.sql
+++ data/toolkit/sql/0100-func-create-index.sql
@@ -1,7 +1,7 @@
 create or replace procedure toolkit.create_index(tbl varchar, u boolean, cols varchar[]) as
 $$
 declare
-    index_name varchar := format('ndx_%s_%s', replace(tbl,'.','_'), array_to_string(cols, '_'));
+    index_name varchar := format('ndx_%s_%s', replace(tbl, '.', '_'), array_to_string(cols, '_'));
     col        varchar := array_to_string(cols, ',');
     unq        varchar := case when u then 'unique' else '' end;
     sql        varchar := format('create %s index if not exists %s on %s (%s)', unq, index_name, tbl, col);
@@ -20,22 +20,20 @@ $$ language plpgsql;
 create or replace procedure toolkit.test_create_index() as
 $$
 declare
-    result text:='';
-    tableName   varchar   := 'test_table';
-    columns varchar[] := ARRAY ['foo'];
-    index_name varchar := format('ndx_%s_%s', replace(tableName,'.','_'), array_to_string(columns, '_'));
+    result     text      := '';
+    tbl_name   varchar   := 'test_table';
+    cols       varchar[] := ARRAY ['foo'];
+    index_name varchar   := format('ndx_%s_%s', replace(tbl_name, '.', '_'), array_to_string(cols, '_'));
 begin
-    create table test_table(
-        foo text not null
-    );
-    call toolkit.create_index(tableName, false, columns);
-    result=(select indexdef from pg_catalog.pg_indexes where indexname=index_name);
+    execute format('create table %s (%s varchar not null);', tbl_name, array_to_string(cols, '_'));
+    call toolkit.create_index(tbl_name, false, cols);
+    result = (select indexdef from pg_catalog.pg_indexes where indexname = index_name);
     if result <> 'CREATE INDEX ndx_test_table_foo ON public.test_table USING btree (foo)' then
         raise exception 'index not structured as expected: ''%''', result;
     end if;
     --clean-up
     execute format('drop index %s', index_name);
-    drop table test_table;
+    execute format('drop table %s', tbl_name);
     drop procedure toolkit.test_create_index;
 end
 $$ language plpgsql;
