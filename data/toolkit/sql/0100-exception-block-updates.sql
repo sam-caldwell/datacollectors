5231fd3259e05c2e041c26e585be490f0122a492
Sam Caldwell <github@samcaldwell.net>
2023-01-22 21:43:36 -0600

    more docs

--- data/toolkit/sql/0100-exception-block-updates.sql
+++ data/toolkit/sql/0100-exception-block-updates.sql
@@ -1,3 +1,8 @@
+/*
+ * This function will throw an exception when executed by a trigger.
+ * When updates are blocked on a table, this function is used to
+ * enforce the block.
+ */
 create or replace function toolkit.exception_block_updates() RETURNS trigger AS
 $$
 begin
