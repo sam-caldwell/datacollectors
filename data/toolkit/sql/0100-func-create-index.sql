33f944f7bbc9f052edb30d0685a2c42f9f9f134e
Sam Caldwell <github@samcaldwell.net>
2023-01-22 18:14:59 -0600

    add unit test for toolkit.create_index

--- data/toolkit/sql/0100-func-create-index.sql
+++ data/toolkit/sql/0100-func-create-index.sql
@@ -9,3 +9,46 @@ begin
     execute sql;
 end
 $$ language plpgsql;
+/*
+ *  ------------------------------------------------------------------------------
+ *  Unit Tests
+ *  ------------------------------------------------------------------------------
+ */
+/*
+ * test the test_create_index() function works without errors.
+ */
+create or replace procedure toolkit.test_create_index() as
+$$
+declare
+    result text:='';
+    tableName   varchar   := 'test_table';
+    columns varchar[] := ARRAY ['foo'];
+    index_name varchar := format('ndx_%s_%s', replace(tableName,'.','_'), array_to_string(columns, '_'));
+begin
+    create table test_table(
+        foo text not null
+    );
+    call toolkit.create_index(tableName, false, columns);
+--     result=(select indexdef from pg_catalog.pg_indexes where indexname=eName);
+--     if result <> 'CREATE INDEX test_index ON public.test_table USING btree (foo)' then
+--         raise exception 'index not structured as expected';
+--     end if;
+    --clean-up
+    execute format('drop index %s', index_name);
+    drop table test_table;
+    drop procedure toolkit.test_create_index;
+end
+$$ language plpgsql;
+/*
+ *  ------------------------------------------------------------------------------
+ *  Running tests
+ *     All unit tests should be above this section
+ *  ------------------------------------------------------------------------------
+ */
+do
+$$
+    begin
+        raise notice 'test: toolkit.create_index() starting';
+        call toolkit.test_create_index();
+    end
+$$ language plpgsql;
